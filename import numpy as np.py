e8b0c1d38442591f13c44eda5fc76d070eee3ec8
NovikNaji <aytalnovikov2001@mail.ru>
2022-07-21 14:55:01 +0900

    2

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,16 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import math
+import array as arr 
+
+a=list(input().lower())
+n=len(a)
+k=0
+b=''
 
 
-f = open('data.txt', 'r')
-d = open('dawlw.txt', 'w')
 
-with f as file:
-    w=file.read()
 
-with d as file:
-    file.write(w)
-print(w)
-f.close()
-d.close()
+
+for i in range(n):
+    k=0
+    for j in range(n):
+        if(a[i]==a[j]):
+            k+=1
+    if(k==1):
+        b+='('
+    else:
+        b+=')'
+print(n)
+print(b)
+print(a)
